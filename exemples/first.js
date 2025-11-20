062eac31efdad4b90400c7a18b061de297c18ba5
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-11-20 10:26:21 +0100

    first commit

---
+++ exemples/first.js
@@ -0,0 +1,18 @@
+let tab = [3, "nidhal", true];
+
+// for (const element of tab) {
+//   console.log(element);
+// }
+// for (const element in tab) {
+//   console.log(element);
+// }
+
+let o = {
+  prenom: "nidhal",
+  formation: "m2i",
+  age: 40,
+};
+
+for (const key of o) {
+  console.log(key);
+}
