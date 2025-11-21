01a81e7447b8e46fe79f9f6c9da60475d1675217
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-11-21 14:42:38 +0100

    correction tp3

---
+++ TP3/script.js
@@ -0,0 +1,56 @@
+const inpIntitule = document.getElementById("intitule");
+const inpMontant = document.getElementById("montant");
+const btnAjouter = document.getElementById("btn-ajouter");
+const listeDepenses = document.getElementById("list-dep");
+const spanTotal = document.getElementById("total-dep");
+const filtrePrix = document.getElementById("filtre-prix");
+
+let afficherPasDeDepenses = false;
+let total = 0;
+btnAjouter.addEventListener("click", () => {
+  if (afficherPasDeDepenses) {
+    listeDepenses.innerHTML = "";
+    afficherPasDeDepenses = false;
+  }
+
+  let newLi = document.createElement("li");
+  newLi.className = "list-group-item"; // ou passer par classList
+
+  let newText = document.createTextNode(
+    `${inpIntitule.value} : ${inpMontant.value}€`
+  );
+
+  newLi.appendChild(newText);
+  listeDepenses.appendChild(newLi);
+
+  total += +inpMontant.value;
+  spanTotal.textContent = `${total}€`;
+});
+
+filtrePrix.addEventListener("input", (e) => {
+  for (const child of listeDepenses.children) {
+    let t = child.textContent.split(" ");
+    // Ou     if(Number(t[2] < Number(filtrePrix.value)))
+    if (e.target.value.length != 0) {
+      if (Number(t[2]) >= Number(e.target.value)) {
+        // child.style.backgroundColor = "red";
+        child.classList.remove("list-group-item-success");
+        child.classList.add("list-group-item-danger");
+      } else {
+        // child.style.backgroundColor = "green";
+        child.classList.remove("list-group-item-danger");
+        child.classList.add("list-group-item-success");
+      }
+    } else child.style.backgroundColor = "white";
+  }
+});
+
+document.getElementById("inpTag").addEventListener("keyup", (e) => {
+  if (e.code == "Space") {
+    let newSpan = document.createElement("span");
+    newSpan.className = "badge text-bg-secondary m-2";
+    newSpan.textContent = `#${document.getElementById("inpTag").value}`;
+    document.getElementById("tags").appendChild(newSpan);
+    document.getElementById("inpTag").value = "";
+  }
+});
