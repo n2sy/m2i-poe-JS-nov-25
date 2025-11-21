c661750db98ee73a96942241c6a2a254ced62ef7
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-11-21 15:44:36 +0100

    orientée prototype vs oriente objet

--- exemples/first.js
+++ exemples/first.js
@@ -1,11 +1,21 @@
-let tab = [3, "nidhal", true];
+// let tab = [3, "nidhal", true];
 
-// for (const element of tab) {
-//   console.log(element);
+// // for (const element of tab) {
+// //   console.log(element);
+// // }
+// // for (const element in tab) {
+// //   console.log(element);
+// // }
+
+// for (const key of o) {
+//   console.log(key);
 // }
-// for (const element in tab) {
-//   console.log(element);
-// }
+
+// let a = function (x, y) {
+//   return x * y;
+// };
+
+// a(4, 5);
 
 let o = {
   prenom: "nidhal",
@@ -13,6 +23,24 @@ let o = {
   age: 40,
 };
 
-for (const key of o) {
-  console.log(key);
+let o1 = {
+  prenom: "nidhal",
+  formation: "m2i",
+  age: 40,
+};
+
+function Etudiant(prenom, age) {
+  this.prenom = prenom;
+  this.age = age;
+}
+
+let e1 = new Etudiant("nidhal", 40);
+e1.numTel = "0712121";
+let e2 = new Etudiant("baptiste", 19);
+delete e2.age;
+
+console.log(e1, e2);
+
+class Etudiant {
+  constructor() {}
 }
