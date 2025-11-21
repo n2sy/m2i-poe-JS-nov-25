af37eed40c3e593651e07913229acf47e91fc6d9
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-11-21 09:46:53 +0100

    correction ex3 et début ex5 serie 2

---
+++ TP2/ex5.js
@@ -0,0 +1,17 @@
+let listeCours = [
+  {
+    id: 1,
+    intitule: "Angular",
+    niveau: "débutant",
+  },
+  {
+    id: 2,
+    intitule: "React",
+    niveau: "intermédiaire",
+  },
+  {
+    id: 3,
+    intitule: "Next",
+    niveau: "débutant",
+  },
+];
