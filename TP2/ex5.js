07eea52f110b35a0278fc170e5f0906756cf0f75
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-11-21 11:30:10 +0100

    correction ex5 serie 2

--- TP2/ex5.js
+++ TP2/ex5.js
@@ -15,3 +15,25 @@ let listeCours = [
     niveau: "débutant",
   },
 ];
+
+let codeAInserer = "";
+divInfos = document.getElementById("cours");
+for (const cours of listeCours) {
+  codeAInserer = codeAInserer.concat(
+    `
+        <ul id=${cours.id}>
+        <li>${cours.intitule}</li>
+        <li>${cours.niveau}</li>
+        <li>0</li>
+        <button onclick="ajouterLike(${cours.id})">❤️</button>
+        </ul>
+        `
+  );
+}
+
+divInfos.innerHTML = codeAInserer;
+
+function ajouterLike(coursId) {
+  document.getElementById(coursId).children[2].textContent =
+    Number(document.getElementById(coursId).children[2].textContent) + 1;
+}
